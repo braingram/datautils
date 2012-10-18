7fbacd5e65b9efdc4fda777749efa74c43c6dd9c
Brett Graham <graham@rowland.harvard.edu>
2012-10-18 14:53:31 -0400

    v0.1

--- setup.py
+++ setup.py
@@ -223,7 +223,7 @@ if __name__ == '__main__':
     else:
         setuptools.setup(
             name=package_name,
-            version='dev',
+            version='0.1',
             packages=packages,
             scripts=scripts,
 
