206ecd786e4d0fda6ee6829bbc6ec653732c9368
Brett Graham <graham@rowland.harvard.edu>
2012-10-22 12:44:45 -0400

    bumping version

--- setup.py
+++ setup.py
@@ -223,7 +223,7 @@ if __name__ == '__main__':
     else:
         setuptools.setup(
             name=package_name,
-            version='0.1.2',
+            version='0.1.3',
             packages=packages,
             scripts=scripts,
 
