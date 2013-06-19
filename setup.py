abfc4cbfecbd11dcce4480affae8bd17982a2ea5
Brett Graham <brettgraham@gmail.com>
2013-06-18 22:38:07 -0400

    version bump to 1.0

--- setup.py
+++ setup.py
@@ -223,7 +223,7 @@ if __name__ == '__main__':
     else:
         setuptools.setup(
             name=package_name,
-            version='0.1.6',
+            version='1.0.0',
             packages=packages,
             scripts=scripts,
 
