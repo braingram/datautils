024cf5f37f36d1db8285ab0cab5a0919ce8bfaee
Brett Graham <brettgraham@gmail.com>
2013-07-08 12:32:58 -0400

    adding function lookup cod

--- datautils/np/__init__.py
+++ datautils/np/__init__.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python
 
 from . import convert
+from . import flookup
 from . import mask
 from . import query
 
 from .mask import mask_array
 from .query import query_array
 
-__all__ = ['convert', 'mask', 'mask_array', 'query', 'query_array']
+__all__ = ['convert', 'mask', 'mask_array', 'query', 'query_array', 'flookup']
