8d0259ede28efe9b6e291cb2f39b552dff8bbd8f
Brett Graham <brettgraham@gmail.com>
2016-03-29 19:33:44 -0400

    adding timed serf test

--- datautils/structures/mp/test.py
+++ datautils/structures/mp/test.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python
 
 import time
+import os
 
 from .lord import Lord
 from .serf import Serf
+from .serf import TimedSerf
 
 
-class SleepySerf(Serf):
+class SleepySerf(TimedSerf):
     def sleep(self, seconds=1):
         time.sleep(seconds)
 
@@ -28,3 +30,15 @@ def test_sleepy_serf():
             break
     print "stopping"
     l.stop()
+
+
+def test_timed_sleepy_serf():
+    l = Lord()
+    l.start(SleepySerf)
+    l.send('set_log', 'sleep.log')
+    l.send('sleep', 1.)
+    l.stop()
+    with open('sleep.log', 'r') as f:
+        for l in f:
+            print l
+    os.remove('sleep.log')
